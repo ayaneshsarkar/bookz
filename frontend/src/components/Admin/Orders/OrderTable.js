fce4b9fa534d51f0a4d0ea3aa4c13301437104f7
Ayanesh Sarkar <ayaneshsarkar101@gmail.com>
2021-09-03 15:54:29 +0530

    Initial Commit :heavy_check_mark:

--- frontend/src/components/Admin/Orders/OrderTable.js
+++ frontend/src/components/Admin/Orders/OrderTable.js
@@ -7,11 +7,20 @@ const OrderTable = ({ orders, user }) => {
     return orders.map((order, i) => {
       return (
         <tr key={i}>
-          <td className="imp"><Link to={`/admin/order/${order.id}`}>{ i + 1 }</Link></td>
-          <td>{ order.first_name + ' ' + order.last_name }</td>
+          <td className="d-none table-sm">
+            <div className="limited m-1 info">
+              Buyer: <span>{ order.first_name + ' ' + order.last_name }</span>
+            </div>
+            <div className="limited m-1 info">
+              Order No. <span>{ order.invoice_id.toUpperCase() }</span>
+            </div>
+
+          </td>
+          <td className="lgmd imp"><Link to={`/admin/order/${order.id}`}>{ i + 1 }</Link></td>
+          <td className="lgmd">{ order.first_name + ' ' + order.last_name }</td>
           <td>{ order.invoice_id.toUpperCase() }</td>
 
-          {user.type &&<td>
+          {user.type && <td>
             <svg>
               <use xlinkHref={`${Sprite}#trash-2`}></use>
             </svg>
@@ -25,9 +34,10 @@ const OrderTable = ({ orders, user }) => {
     <>
       <thead>
         <tr>
-          <th>#</th>
-          <th>User</th>
-          <th>Order No.</th>
+          <th className="d-none table-sm">Order Details</th>
+          <th className="lgmd">#</th>
+          <th className="lgmd">User</th>
+          <th className="lgmd">Order No.</th>
           {user.type && <th></th>}
         </tr>
       </thead>
